3696a19030d12dac7fe4f89b924f5095f4f30dac
umer <choudhry00641@gmail.com>
2025-10-23 23:40:53 +0500

    new

--- components/footer.tsx
+++ components/footer.tsx
@@ -76,7 +76,7 @@ export function Footer() {
               rel="noopener noreferrer"
               className="underline underline-offset-4 transition-colors hover:text-blue-900 hover:bg-blue-50 rounded px-1"
             >
-               AK.
+               akmarketingagency
             </a>
             </p>
         </div>
